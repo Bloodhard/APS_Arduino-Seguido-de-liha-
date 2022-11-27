40d5e9c99186950baa192510e085f6d3f0bf838f
Luan Duarte <luanguimaraes031101@gmail.com>
2022-11-27 15:08:17 -0300

    Adição da livraria TimeOne | Função de parada

--- APSARDUINOSensores.ino
+++ APSARDUINOSensores.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>  //Esta biblioteca permite que você se comunique com dispositivos I2C/TWI. Nas placas Arduino com o layout R3 (pinagem 1.0), o SDA (linha de dados) e SCL (linha de clock) estão nos cabeçalhos dos pinos próximos ao pino AREF. O Arduino Due possui duas interfaces I2C/TWI SDA1 e SCL1 próximas ao pino AREF e uma adicional nos pinos 20 e 21.
 #include "Adafruit_TCS34725.h" //biblioteca do sensor RGB TCS34725
+#include <TimerOne.h>
 
 int SensorD = A1;  // sensor de linha da direita
 int SensorC = 2;          // sensor de linha do meio
@@ -32,8 +33,14 @@ void setup() {
   //Sensor RGB/LED
   Serial.begin(9600);  //Iniciamos o monitor serial na taxa de transmissão de 9600
   pinMode(LDR, OUTPUT);
-}
 
+}
+    void parar(){
+      analogWrite(MDF, 0);
+      analogWrite(MEF, 0);
+      analogWrite(MDT, 0);
+      analogWrite(MET, 0);
+    }
 
   void loop() {
     /*
@@ -63,7 +70,8 @@ void setup() {
       }
     }
     */
- 
+   
+    
     if (digitalRead(SensorE) == LOW && digitalRead(SensorD) == LOW && digitalRead(SensorC) == LOW) {  // detectou a linha branca nos 3 sensores
       analogWrite(MDF, 0);
       analogWrite(MEF, 0);
@@ -85,11 +93,13 @@ void setup() {
       analogWrite(MDT, 0);
       analogWrite(MET, 0);
     } else {  // 
-      analogWrite(MDF, 0);
-      analogWrite(MEF, 0);
+      analogWrite(MDF, POT);
+      analogWrite(MEF, POT);
       analogWrite(MDT, 0);
       analogWrite(MET, 0);
-    }
+      Timer1.initialize(2000000);
+      Timer1.attachInterrupt(parar);
+    } 
 
 
   delay(20);
